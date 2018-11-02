98293f87b13bf02db5a5578ac2d620a67b040d32
Manish Kumar Sharma <loclghst@gmail.com>
2018-11-02 16:14:07 +0530

    Refactor App to a functional component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React , {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBtRz7tuY0VHfs73B_af3eKE1YTTl12hbI';
 
+class  App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {videos: []};
 
-
-//create  a component
-
-const App = () => {
-	return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        YTSearch({key : API_KEY, term:'apple'},(videos) => {
+            this.setState({videos});
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
-
-
-//shove it on to the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
