35bd41fcea5968db153e6f801511aa1eb400fd4f
Manish Kumar Sharma <loclghst@gmail.com>
2018-11-02 17:19:28 +0530

    Make video list clickable

--- src/index.js
+++ src/index.js
@@ -10,18 +10,26 @@ const API_KEY = 'AIzaSyBtRz7tuY0VHfs73B_af3eKE1YTTl12hbI';
 class  App extends Component {
     constructor(props){
         super(props);
-        this.state = {videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key : API_KEY, term:'apple'},(videos) => {
-            this.setState({videos});
+        YTSearch({key : API_KEY, term:'toys in japan'},(videos) => {
+            this.setState({
+                videos,
+                selectedVideo: videos[0],
+            });
         });
     }
     render(){
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]} />
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                onVideoSelect ={(selectedVideo) => {this.setState({selectedVideo})}}
+                videos={this.state.videos} />
             </div>
         );
     }
