79a02423998c07605bebc08d30802c9f9d1e1060
Manish Kumar Sharma <loclghst@gmail.com>
2018-11-02 16:37:58 +0530

    Fix the video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) =>{
     const VideoList = props.videos.map((video) =>{
-        return <VideoListItem video={video} />
+        return <VideoListItem key={video.etag} video={video} />
     });
 
     return (
