502b06681c1b5517728532af5e8fa0b76ffd13dc
Manish Kumar Sharma <loclghst@gmail.com>
2018-11-02 17:52:42 +0530

    Add search funtionality

--- src/index.js
+++ src/index.js
@@ -14,18 +14,24 @@ class  App extends Component {
             videos: [],
             selectedVideo: null
         };
+        this.videoSearch('toys in japan')
 
-        YTSearch({key : API_KEY, term:'toys in japan'},(videos) => {
+    }
+
+    videoSearch(term){
+        YTSearch({key : API_KEY, term:term},(videos) => {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0],
             });
         });
+
     }
+
     render(){
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={(term) => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                 onVideoSelect = {(selectedVideo) => {this.setState({selectedVideo})}}
